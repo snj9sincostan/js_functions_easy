fecca0455f3057cf2a6d4a87f73f021809a1aef4
Shinji Nishihara <snmal2278@gmail.com>
2021-10-02 20:59:47 +0900

    [update] 関数の作成

---
+++ js/zipObject.js
@@ -0,0 +1,12 @@
+const zipObject = (props = [], values) => {
+  const zippedObject = {};
+  for (let i = 0; i < props.length; i++) {
+    const prop = props[i];
+    const value = values[i];
+    zippedObject[prop] = value;
+  }
+
+  return zippedObject;
+};
+
+console.log(zipObject(["a", "b"], [1, 2]));
